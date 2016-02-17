68762ae517a859d672ed7e3c9d061e1548078ec3
Jake Weinstein <xboxlover360@gmail.com>
2016-02-17 18:27:47 -0500

    bacon: fix gesture node
    Change-Id: Id9f700a4dd8dfb73d488290868bb326220ee0776

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -83,8 +83,8 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 BOARD_GLOBAL_CFLAGS += -DCAMERA_VENDOR_L_COMPAT
 TARGET_HAS_LEGACY_CAMERA_HAL1 := true
 
-# Double tap to wake
-TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
+# Off screen gestures
+TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/gesture_enable"
 
 # Enable dexpreopt to speed boot time
 ifeq ($(HOST_OS),linux)
