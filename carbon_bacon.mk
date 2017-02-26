2beb7d00bfa59323df555603b52783b71e92c06e
darjwx <darjwx@gmail.com>
2017-02-26 17:19:42 +0100

    bacon: add maintainer

--- carbon_bacon.mk
+++ carbon_bacon.mk
@@ -47,3 +47,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=oneplus/bacon/A0001:6.0.1/MHC19Q/ZNH2KAS1KN:user/release-keys \
     PRIVATE_BUILD_DESC="bacon-user 6.0.1 MHC19Q ZNH2KAS1KN release-keys"
 endif
+
+# Maintainer
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.carbon.maintainer="darjwx"
