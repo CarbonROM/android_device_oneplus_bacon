26d78de22fc44cb4444c734dc14da334a80087c7
darjwx <darjwx@gmail.com>
2017-05-11 17:50:36 +0200

    [WIP] bacon: ramdisk: add support for spectrum
    *Credits to @frap129
    
    Change-Id: Ib0d2eb692db5f67e6ac0c44c58a4b893d79ac676

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -59,3 +59,21 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
+# Spectrum init files
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.spectrum.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.spectrum.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.spectrum.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.spectrum.sh
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
