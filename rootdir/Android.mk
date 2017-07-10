ad64f612f5315f5d90b86967ef255f1a5ad2a110
darjwx <darjwx@gmail.com>
2017-07-10 16:48:14 +0200

    bacon: introduce init.crkernel.rc
    * Tune also some values (Based off @Setting.Out script)
    * You can find here the custom configs of carbon,
      such as autosmp and different governor's values.
    
    Change-Id: Ie1879efcaf8f7931a2a0220b744145e97f554cfb

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -59,3 +59,12 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
+# Carbon init files
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.crkernel.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.crkernel.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
