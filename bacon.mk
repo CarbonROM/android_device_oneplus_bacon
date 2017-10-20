db8049e3ff29dd588d3a92fd0d6af80926219161
jrior001 <jriordan001@gmail.com>
2017-10-19 23:46:26 -0400

    bacon: Use new legacy HIDL implementations for camera
    Change-Id: I5482727eda07ca74457ada903388bfbf10fae680

--- bacon.mk
+++ bacon.mk
@@ -79,8 +79,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Camera
 PRODUCT_PACKAGES += \
-    camera.device@1.0-impl \
-    android.hardware.camera.provider@2.4-impl \
+    camera.device@1.0-impl-legacy \
+    android.hardware.camera.provider@2.4-impl-legacy \
     camera.bacon
 
 # Charger
