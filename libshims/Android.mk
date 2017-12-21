e79e44ddd8f22f4cae7f8ebf7ed271fc67fbdeb3
Michael Bestas <mkbestas@lineageos.org>
2017-12-21 18:10:01 +0200

    bacon: Move camera shims to vendor
    * Also cleanup flags while we are at it.
    
    Change-Id: I1eedfdf5ed112b33bdb7dfac146eb33ba9796e0d

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -5,6 +5,8 @@ LOCAL_SRC_FILES := camera_shim.cpp
 LOCAL_SHARED_LIBRARIES := libui
 LOCAL_MODULE := libshim_camera
 LOCAL_MODULE_TAGS := optional
+LOCAL_PROPRIETARY_MODULE := true
+
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
@@ -12,4 +14,6 @@ LOCAL_SRC_FILES := cameraclient_shim.cpp
 
 LOCAL_MODULE := libshim_cameraclient
 LOCAL_MODULE_TAGS := optional
+LOCAL_PROPRIETARY_MODULE := true
+
 include $(BUILD_SHARED_LIBRARY)
