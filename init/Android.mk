7158fd188047d40fa7156e95dffe489b4d7f2bc3
darjwx <darjwx@gmail.com>
2017-12-30 16:23:43 +0100

    bacon: init: fix some includes
    Change-Id: Ide628defeb9473cceec67d395432a43f949a7290

--- init/Android.mk
+++ init/Android.mk
@@ -3,7 +3,11 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES := system/core/init
+LOCAL_C_INCLUDES := \
+    system/core/init \
+    system/core/base/include \
+    external/selinux/libselinux/include
+
 LOCAL_CFLAGS := -Wall
 LOCAL_STATIC_LIBRARIES := libbase
 LOCAL_SRC_FILES := init_bacon.cpp
