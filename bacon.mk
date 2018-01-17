1857e25b6b1d9b2effe00a12ccaa4d5bf5d9bc8b
Michael Bestas <mkbestas@lineageos.org>
2018-01-17 18:50:19 +0200

    bacon: Move NFC configs to vendor
    Change-Id: I294cf937d6b9adc317c6d165a78d642f2907da48

--- bacon.mk
+++ bacon.mk
@@ -232,8 +232,8 @@ PRODUCT_PACKAGES += \
     Tag
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
-    $(LOCAL_PATH)/configs/libnfc-brcm.conf:system/etc/libnfc-brcm.conf
+    $(LOCAL_PATH)/configs/libnfc-nxp.conf:system/vendor/etc/libnfc-nxp.conf \
+    $(LOCAL_PATH)/configs/libnfc-brcm.conf:system/vendor/etc/libnfc-brcm.conf
 
 # Disable Treble properties
 PRODUCT_PROPERTY_OVERRIDES += \
