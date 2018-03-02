1ea1763b081e6c862e3b2244363f349ba7017909
Kshitij Gupta <kshitijgm@gmail.com>
2018-03-02 19:34:43 +0530

    bacon: Import and adapt camera provider
    - Import from hardware/interfaces/camera/provider/2.4/default
    - Remove vndbinder usage
    
    Change-Id: I33dd83e735be395a1bfd6df54130ff8544fabd00
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- bacon.mk
+++ bacon.mk
@@ -79,8 +79,9 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
     camera.device@1.0-impl \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service.bacon \
     camera.msm8974 \
     Snap
 
